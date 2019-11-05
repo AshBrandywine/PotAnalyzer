0208ca3686175650ec44dbb6011c552d6c7ee51d
Barnes <Chase.Barnes@henryschein.com>
2019-11-05 12:42:49 -0700

    Added fully functional leet permutation generator, slimmed down leet dictionary

--- PotAnalyzer.py
+++ PotAnalyzer.py
@@ -33,11 +33,11 @@ try:
             line_split = line.strip().split(":")
             if len(line_split) < 2:
                 continue
-            password = line_split[1]
+            password = line_split[len(line_split)-1]
             password_set.add(password)
             mask = passwordtools.get_mask(password)
             mask_key = "".join(mask)
-            if masks.__contains__(mask_key):
+            if mask_key in masks.keys():
                 masks[mask_key] = masks[mask_key] + 1
             else:
                 masks[mask_key] = 1
--- passwordtools.py
+++ passwordtools.py
@@ -14,41 +14,41 @@ special_pool = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
 # http://www.gamehouse.com/blog/leet-speak-cheat-sheet/
 leet_dict = {"a": ("4", "@"),
-             "b": ("8", "l3", "13", "6"),
-             "c": ("[", "{", "<", "("),
-             "d": ("t", ")", "[)", "1)", "l)"),
-             "e": ("3", "&"),
-             "f": ("ph"),
-             "g": ("&", "6", "gee"),
-             "h": ("#"),
-             "i": ("1", "l", "!", "eye"),
+             "b": ("8",),
+             # "c": (),
+             # "d": (),
+             "e": ("3",),
+             "f": ("ph",),
+             "g": ("6",),
+             # "h": (),
+             "i": ("1", "l"),
              "j": ("1", "l"),
              # "k": (),
              "l": ("1", "7"),
-             "m": ("/v\\", "/V\\", "/\\/\\"),
+             # "m": (),
              # "n": (),
-             "o": ("0", "p", "u", "Q", "oh"),
+             "o": ("0", "p", "u"),
              # "p": (),
              # "q": (),
              # "r": (),
-             "s": ("5", "$", "z", "Z"),
-             "t": ("7"),
+             "s": ("5", "$"),
+             "t": ("7",),
              # "u": (),
-             "v": ("\\/"),
-             "w": ("\\/\\/", "VV", "vv"),
+             # "v": (),
+             # "w": (),
              # "x": (),
              "y": ("j", "i", "7"),
-             "z": ("2", "7", "s", "S")}
+             "z": ("2", "7")}
 
 
 def get_mask(password):
     mask = []
     for char in password:
-        if alpha_lower_pool.__contains__(char):
+        if char in alpha_lower_pool:
             mask.append(alpha_lower_mask)
-        elif alpha_upper_pool.__contains__(char):
+        elif char in alpha_upper_pool:
             mask.append(alpha_upper_mask)
-        elif digit_pool.__contains__(char):
+        elif char in digit_pool:
             mask.append(numeric_mask)
         else:
             mask.append(special_mask)
@@ -114,18 +114,12 @@ def get_remove_derivatives(password):
         derivative_set.add("".join(derivative_builder))
     return derivative_set
 
-# TODO - These need to be more than just 1 substitute at a time
+
 def get_leet_derivatives(password):
     derivative_set = set()
-    for i in range(len(password)):
-        char = password[i].lower()
-        if char in leet_dict.keys():
-            substitutes = leet_dict[char]
-            for sub in substitutes:
-                derivative_builder = list(password)
-                del derivative_builder[i]
-                derivative_builder.insert(i, sub)
-                derivative_set.add("".join(derivative_builder))
+    leet_generator = LeetGenerator(password)
+    while leet_generator.has_next:
+        derivative_set.add(leet_generator.get_next_permutation())
     return derivative_set
 
 
@@ -138,4 +132,78 @@ def get_all_derivatives(password, mask=None):
     derivative_set.update(get_remove_derivatives(password))
     derivative_set.update(get_leet_derivatives(password))
     return derivative_set
+
+
+class CascadeCounter:
+    def __init__(self, max_value):
+        self.next_counter = None
+        self.value = 0
+        self.max_value = max_value
+
+    def increment(self):
+        self.value += 1
+        if self.value > self.max_value:
+            self.value = 0
+            if self.next_counter is not None:
+                self.next_counter.increment()
+
+    def is_max(self):
+        return self.value == self.max_value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str("(" + str(self.value) + "/" + str(self.max_value) + ")")
+
+
+class LeetGenerator:
+    def __init__(self, password):
+        self.has_next = True
+        self.omni_mask = self.__getmask__(password)
+        self.counter_list = []
+        self.counter_head = None
+        self.counter_tail = None
+        for i in range(len(self.omni_mask)):
+            mask_chars = self.omni_mask[i]
+            max_value = len(mask_chars) - 1
+            self.__appendcounter__(max_value)
+
+    def __appendcounter__(self, max_value):
+        counter = CascadeCounter(max_value)
+        self.counter_list.append(counter)
+        if self.counter_head is None:
+            self.counter_head = counter
+            self.counter_tail = counter
+        else:
+            self.counter_tail.next_counter = counter
+            self.counter_tail = counter
+
+    def __getmask__(self, password):
+        omni_mask = []
+        for char in password:
+            if char in leet_dict.keys():
+                omni_mask.append((char,) + leet_dict[char])
+            else:
+                omni_mask.append((char,))
+        return omni_mask
+
+    def __increment__(self):
+        self.counter_head.increment()
+        for counter in self.counter_list:
+            if counter.value != 0:
+                return
+        self.has_next = False
+
+    def get_next_permutation(self):
+        permutation_builder = []
+        for i in range(len(self.omni_mask)):
+            char_options = self.omni_mask[i]
+            current_index = self.counter_list[i].value
+            char = char_options[current_index]
+            permutation_builder.append(char)
+        self.__increment__()
+        permutation = "".join(permutation_builder)
+        print(permutation)
+        return "".join(permutation_builder)
 
